4245762716c52c27f1c4068aa1ed7941efb31637
IrfanCatovic <catovicc84@gmail.com>
2025-10-12 17:06:45 +0200

    Css for Racun

--- src/Racun.js
+++ src/Racun.js
@@ -1,19 +1,25 @@
+import "./Racun.css";
+
 export default function Racun({ trenutniRacun, onUkloni, onZavrsiKupovinu }) {
   const ukupno = trenutniRacun.reduce((s, a) => s + a.Cena, 0);
 
   return (
-    <div>
+    <div className="racun-container">
       <h2>Trenutni račun</h2>
       <ul>
         {trenutniRacun.map((a, i) => (
           <li key={i}>
-            {a.Ime} - {a.Cena} RSD
+            <span>
+              {a.Ime} - {a.Cena} RSD
+            </span>
             <button onClick={() => onUkloni(i)}>−</button>
           </li>
         ))}
       </ul>
       <p>Ukupno: {ukupno} RSD</p>
-      <button onClick={onZavrsiKupovinu}>Završi kupovinu</button>
+      <button className="zavrsi-button" onClick={onZavrsiKupovinu}>
+        Završi kupovinu
+      </button>
     </div>
   );
 }
