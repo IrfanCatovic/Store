4da5394f4475d2ba839be3689b8751f85fbddc90
IrfanCatovic <catovicc84@gmail.com>
2025-10-12 16:53:29 +0200

    Css style za artikal

--- src/Artikal.js
+++ src/Artikal.js
@@ -1,12 +1,15 @@
+import "./Artikal.css";
+
 export default function Artikal({ ime, cena, onDodaj }) {
   return (
-    <>
-      <div>
-        <p>
-          {ime}: {cena}{" "}
-          <button onClick={() => onDodaj({ ime, cena })}>+</button>
-        </p>
+    <div className="artikal-container">
+      <div className="artikal-info">
+        <span className="artikal-ime">{ime}</span>
+        <span className="artikal-cena">{cena} RSD</span>
       </div>
-    </>
+      <button className="artikal-button" onClick={() => onDodaj({ ime, cena })}>
+        +
+      </button>
+    </div>
   );
 }
