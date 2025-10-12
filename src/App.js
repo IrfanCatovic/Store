572707f3e752da8109edf8888beca08c78002c84
IrfanCatovic <catovicc84@gmail.com>
2025-10-12 13:30:10 +0200

    Prodavnica

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Artikal from "./Artikal";
 
-function App() {
+const Artikli = [
+  {
+    Id: 1,
+    Ime: "Majonez",
+    Cena: 120,
+  },
+  {
+    Id: 2,
+    Ime: "Kečap",
+    Cena: 100,
+  },
+  {
+    Id: 3,
+    Ime: "Hleb",
+    Cena: 60,
+  },
+];
+
+export default function App() {
+  const [listaArtikala, setListaArtikala] = useState(Artikli);
+  const [artikal, setArtikal] = useState({ Ime: "", Cena: "" });
+
+  function handleAddItem(e) {
+    e.preventDefault();
+
+    if (!artikal.Ime || !artikal.Cena) {
+      alert("Unesite Ime i cenu artikla");
+      return;
+    }
+
+    const noviArtikal = {
+      Id: listaArtikala.length + 1,
+      Ime: artikal.Ime,
+      Cena: artikal.Cena,
+    };
+
+    setListaArtikala([...listaArtikala, noviArtikal]);
+    setArtikal({ Ime: "", Cena: "" });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleAddItem}>
+        <input
+          value={artikal.Ime}
+          placeholder="Upisite ime artikla"
+          onChange={(e) => setArtikal({ ...artikal, Ime: e.target.value })}
+        />
+        <input
+          value={artikal.Cena}
+          placeholder="Upisite cenu"
+          onChange={(e) =>
+            setArtikal({ ...artikal, Cena: Number(e.target.value) })
+          }
+        />
+        <button type="submit">Dodaj</button>
+      </form>
+      {listaArtikala.map((a) => (
+        <li key={a.Id}>
+          <Artikal ime={a.Ime} cena={a.Cena} />
+        </li>
+      ))}
     </div>
   );
 }
-
-export default App;
---
+++ src/Artikal.js
@@ -0,0 +1,9 @@
+export default function Artikal({ ime, cena }) {
+  return (
+    <div>
+      <p>
+        {ime} kosta {cena}
+      </p>
+    </div>
+  );
+}
