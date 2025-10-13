55c1aeb6b6ad9e8e845499afc8d10af4760278b6
IrfanCatovic <catovicc84@gmail.com>
2025-10-13 09:39:06 +0200

    added backend handler for post bill in bill history

--- radnja-backend/main.go
+++ radnja-backend/main.go
@@ -18,24 +18,41 @@ var artikli = []Artikal{
 }
 	var istorijaRacuna [][]Artikal
 	
+	//handler za get artikli
 	func getArtikli(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-type", "application/json")
 	json.NewEncoder(w).Encode(artikli)
 	}
 
+	//handler for set new bill to bill history
+	func postRacun(w http.ResponseWriter, r *http.Request){
+		if r.Method!="POST"{
+			http.Error(w, "Metod nije dozvoljen", http.StatusMethodNotAllowed)
+			return
+		}
+
+		var racun []Artikal
+		err := json.NewDecoder(r.Body).Decode(&racun)
+		if err!= nil{
+			http.Error(w, "Greska pri citanju podataka", http.StatusBadRequest)
+			return
+		}
+
+		istorijaRacuna = append(istorijaRacuna, racun)
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(map[string]string{"Status": "Racun dodat u istoriju"})
+	}
 
 func main() {
 
-
+	//Check if server works
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintln(w, "Server Radi")
 	})
-
-
+	//create route for artikli
 	http.HandleFunc("/artikli", getArtikli)
+	//create route for bill history
+	http.HandleFunc("/racuni", postRacun)
 
-	
-
-	fmt.Println("Server startovan na :8080")
 	http.ListenAndServe(":8080", nil)
 }
