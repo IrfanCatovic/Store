18a5589c68b9fd4d429a40d771cf5392b18e40d6
IrfanCatovic <catovicc84@gmail.com>
2025-10-19 22:42:51 +0200

    added gorila mux

--- radnja-backend/go.mod
+++ radnja-backend/go.mod
@@ -1,3 +1,5 @@
 module radnja-backend
 
 go 1.25.1
+
+require github.com/gorilla/mux v1.8.1 // indirect
--- radnja-backend/main.go
+++ radnja-backend/main.go
@@ -3,15 +3,20 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Server radi")
+	fmt.Fprintln(w, "Server radi sa muxom")
 }
 
 func main() {
-	http.HandleFunc("/", homeHandler)
+	r := mux.NewRouter()
+	
+
+	r.HandleFunc("/", homeHandler)
 	fmt.Println("Pokrenut server")
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", r)
 
 }
