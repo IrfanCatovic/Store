184540c71ea8dc9b8e87f0eecdb2fe0964b2d6af
IrfanCatovic <catovicc84@gmail.com>
2025-10-19 23:14:49 +0200

    CORS done

--- radnja-backend/main.go
+++ radnja-backend/main.go
@@ -7,6 +7,23 @@ import (
 	"github.com/gorilla/mux"
 )
 
+//CORS RESOLVE
+func enableCors(next http.Handler) http.Handler {
+	return http.HandleFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Acces-Cotnrol-Allow-Origin", "*")
+		w.Header().Set("Acces-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Acces-Control-Allow-Headers", "Content-Type, Authorization") 
+
+		if r.Method == "OPTIONS" {
+			w.WritteHeader(http.SattusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Server radi sa muxom")
 }
@@ -15,8 +32,11 @@ func main() {
 	r := mux.NewRouter()
 	
 
-	r.HandleFunc("/", homeHandler)
-	fmt.Println("Pokrenut server")
-	http.ListenAndServe(":8080", r)
+	r.HandleFunc("/", homeHandler).Methods("GET")
+	r.HandleFunc("/artikli", getArtikli).Methods("GET")
+	r.HandleFunc("/racuni", postRacun).Methods("POST")
+	r.HandleFunc("/istorija", getIstorijaRacuna).Methods("GET")
+
+	http.ListenAndServe(":8080", enableCors(r))
 
 }
